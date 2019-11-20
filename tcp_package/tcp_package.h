d3f6aa15efc0f22761735d0fcc14f0b655b95e66
feng <feng@qq.com>
2019-11-20 18:40:12 +0800

    first add

---
+++ tcp_package/tcp_package.h
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<list>
+#icnlude<map>
+#incldue<string>
+#include<sys/socket.h>
+#include"common/common.h"
+#include"common/tcp.h"
+
+using namespace std;
+
+class tcp
+{
+	private:
+	bool block_flag;
+	string ip_addr;
+	int port;
+	
+	public:
+	
+	
+};
